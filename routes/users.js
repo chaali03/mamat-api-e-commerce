963fcf3574bd28f50fae04e5a5129e4591a3394d
chaali03 <zaidan.ali2411@gmail.com>
2025-07-07 17:51:25 +0700

    fix: update users route to use default authenticate import and enhance user management security

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
-const express = require('express');
+import express from 'express';
+import * as profileController from '../controllers/profileController.js';
+import authenticate from '../middleware/auth.js';
+import * as userController from '../controllers/userController.js';
+
 const router = express.Router();
-const profileController = require('../controllers/profileController');
-const { authenticate } = require('../middleware/auth');
 
 // Mendapatkan profil user
 router.get('/profile', authenticate, profileController.getProfile);
@@ -18,4 +20,4 @@ router.get('/order-history', authenticate, profileController.getOrderHistory);
 // Mendapatkan riwayat transaksi - Comment this out for now as it doesn't exist in profileController
 // router.get('/transaction-history', authenticate, userController.getTransactionHistory);
 
-module.exports = router;
+export default router;
